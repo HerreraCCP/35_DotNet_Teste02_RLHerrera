53825da0d8f7a08c0c7b377a01cbc439c01714ec
Rodrigo Herrera Silva <rodrigo-herrera.silva@capgemini.com>
2022-08-30 16:40:03 +0100

    Development

---
+++ TechTest.ClienteApi/Authorization/IdadeMinimaHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ClienteApi.Authorization
+{
+    public class IdadeMinimaHandler : AuthorizationHandler<IdadeMinimaRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+            IdadeMinimaRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == ClaimTypes.DateOfBirth))
+                return Task.CompletedTask;
+
+            var birthday = Convert.ToDateTime(context.User.FindFirst(c => c.Type == ClaimTypes.DateOfBirth).Value);
+            int idade = DateTime.Today.Year - birthday.Year;
+
+            if (birthday > DateTime.UtcNow.AddYears(-idade)) idade--;
+            if (idade >= requirement.IdadeMinima) context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
